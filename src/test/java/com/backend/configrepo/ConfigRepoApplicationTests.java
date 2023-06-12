6b8085434be147974dfcd2005b512a4373dbc34e
Nguyen Tien Dai <daint@vnpay.vn>
2023-06-12 11:37:35 +0700

    init: git repository

---
+++ src/test/java/com/backend/configrepo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.configrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
